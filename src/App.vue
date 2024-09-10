603e4ba5371bfb30daf2e4aecb7a973457d40990
Azizbek0406 <azizbekcoder04@gmail.com>
2024-09-10 20:55:55 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Navbar from '@/comonents/Navbar.vue'; // Ensure the path is correct
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+    <Navbar />
+    <RouterView />
+</template>
